7c364c92a2bf720d8e09855c7f5085bfd9711c69
Kacper <kacpercichy@onet.pl>
2023-01-31 14:02:24 +0100

    Establish DB, change format of properties

---
+++ src/main/java/com/example/PPApp/model/SqlTaskRepository.java
@@ -0,0 +1,12 @@
+package com.example.PPApp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+}
