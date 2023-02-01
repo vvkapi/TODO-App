466557705c98a91c36fa55597d9163f9dc5a0038
Kacper <kacpercichy@onet.pl>
2023-02-01 13:26:38 +0100

    Migartion SQL, FlyWay

--- src/main/java/com/example/PPApp/model/TaskRepository.java
+++ src/main/java/com/example/PPApp/model/TaskRepository.java
@@ -16,7 +16,7 @@ public interface TaskRepository {
 
     Optional<Task> findById(Integer id);
 
-    List<Task> findByDone(@Param("state") boolean done);
+    List<Task> findByDone(boolean done);
 
     Task save(Task entity);
 
