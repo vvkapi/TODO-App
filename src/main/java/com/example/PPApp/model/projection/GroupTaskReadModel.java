b385381f4c03b34d2c2f04b438887bdefbeb813f
Kacper <kacpercichy@onet.pl>
2023-02-01 21:43:08 +0100

    Add GroupTaskReadModel and GroupTaskWriteModel

---
+++ src/main/java/com/example/PPApp/model/projection/GroupTaskReadModel.java
@@ -0,0 +1,29 @@
+package com.example.PPApp.model.projection;
+
+import com.example.PPApp.model.Task;
+
+public class GroupTaskReadModel {
+    private String description;
+    private boolean done;
+
+    GroupTaskReadModel(Task source) {
+        description = source.getDescription();
+        done = source.isDone();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(final boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/java/com/example/PPApp/model/projection/GroupTaskWriteModel.java
@@ -0,0 +1,31 @@
+package com.example.PPApp.model.projection;
+
+import com.example.PPApp.model.Task;
+import com.example.PPApp.model.TaskGroupRepository;
+
+import java.time.LocalDateTime;
+
+public class GroupTaskWriteModel {
+    private String description;
+    private LocalDateTime deadline;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(final LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public Task toTask() {
+        return new Task(description, deadline);
+    }
+}
